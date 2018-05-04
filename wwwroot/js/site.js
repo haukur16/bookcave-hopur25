b9850d9d0af3314caa3ef25f6e6da072e28cff60
gingijons <gingijons@gmail.com>
2018-05-04 23:48:23 +0000

    Nærrumþví klárað layout

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,10 @@
 ﻿// Write your JavaScript code.
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
