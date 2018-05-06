d5177903f982eaa62c93aa9a8f35bf20613e0e8a
gingijons <gingijons@gmail.com>
2018-05-06 21:46:47 +0000

    Quick search tilbúið

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -48,6 +48,12 @@ namespace BookCave.Controllers
             return View(books);
         }
 
+        public IActionResult QuickSearch(string layoutsearch)
+        {
+            var books = _bookService.GetBookByLayoutSearch(layoutsearch);
+            return View(books);
+        }
+
         public IActionResult TopTenBooks(int Id)
         {
             var books = _bookService.GetTopTenBooks(Id);
