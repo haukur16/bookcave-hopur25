9ecd287b5d0742b0ce80c9b6c3d800c7c4feafab
gingijons <gingijons@gmail.com>
2018-05-07 19:22:23 +0000

    Topten listi

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -7,3 +7,17 @@ function empty() {
         return false;
     };
 }
+
+$(function () {
+    $(".index-books").slice(0, 8).show();
+    $("#moreBooksButton").on('click', function (e) {
+        e.preventDefault();
+        $(".index-books:hidden").slice(0, 8).slideDown();
+        if ($(".index-books:hidden").length == 0) {
+            $("#moreBooksButton").fadeOut('slow');
+        }
+        $('html,body').animate({
+            scrollTop: $(this).offset().top
+        }, 1500);
+    });
+});
