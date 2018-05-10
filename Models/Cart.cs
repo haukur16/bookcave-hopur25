e6274b305e73810dc0fa00c21c63c48b35821f48
haukur <haukur16@ru.is>
2018-05-10 11:52:32 +0000

    cart

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -4,6 +4,7 @@ namespace BookCave.Models
 {
     public class Cart
     {
+        public string CartId {get; set; }
         public Book Book {get; set; }
         public int Quantity {get; set; }
     }
